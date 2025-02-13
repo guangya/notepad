9a32f30c3b74b25eebdc0ec5c62b1238ba194f5e
guangya <dmstylecn@qq.com>
2025-02-14 01:09:41 +0800

    测试build

---
+++ assets/2-Dhp2eoac.js
@@ -0,0 +1,1 @@
+import{ak as e,K as c,N as n}from"./index-3Z8Xrm0H.js";const t={};function a(o,r){return n(),c("div",null," Navigation 2 ")}const _=e(t,[["render",a]]);export{_ as default};
---
+++ assets/item-1-ttr68_iT.js
@@ -0,0 +1,1 @@
+import{ak as e,K as t,N as c}from"./index-3Z8Xrm0H.js";const n={};function r(o,a){return c(),t("div",null," Item One ")}const _=e(n,[["render",r]]);export{_ as default};
